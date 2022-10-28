4f1e7aafd9a0e517a78cc693398d334da5472d6c
abuakr-alzubir <107881300+abuakr-alzubir@users.noreply.github.com>
2022-10-28 10:46:07 +0300

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,33 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form action="/search" method="get">
-                        @csrf
-                        <div>
-                          <input type="text" name="word" placeholder="search our products">
-                          <input type="submit" value="search">
-                        </div>
-                        <a style="color: red" href="create_product">Add new product</a>
-                      </form>
-
-
-                      @if(isset($msg))
-                        <div>
-                          <h1>{{$msg}}</h1>
-                        </div>
-                      @elseif(isset($products))
-                        <div>
-                          <h1 style="font-size: 20px; font-weight:bold">The searched items are:</h1>
-                          <ul>
-                          @foreach($products as $product)
-                            <h3> ========================</h3>
-                            <li>{{$product->name}}</li>
-                            <li>{{$product->desc}}</li>
-                            <li>{{$product->price}}</li>
-                          @endforeach
-                          </ul>
-                        </div>
-                      @endif
+                    <h1>Welcome! My name is: your name here</h1>
                 </div>
             </div>
         </div>
