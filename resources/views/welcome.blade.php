2475d315f5ef1ea6d55c2e434426375e38376ac9
abuakr-alzubir <107881300+abuakr-alzubir@users.noreply.github.com>
2022-10-28 15:09:15 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,7 @@
         </style>
     </head>
     <body class="antialiased">
-       <h1>Welcome! My name is: ABUBAKR. This is the Production Environment”.</h1>
+       <h1> “Welcome! My name is: ABUBAKR. This is the Staging Environment”.
+</h1>
     </body>
 </html>
